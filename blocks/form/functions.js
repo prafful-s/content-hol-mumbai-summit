c0fad2f8c2bcb520a041de3d9df2d01b23540b68
prafful-s <151611931+prafful-s@users.noreply.github.com>
2025-05-22 19:05:46 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -6,18 +6,41 @@
  * @return {string}
  */
 function getFullName(firstname, lastname) {
-    return 'full name';
+  return `${firstname} ${lastname}`.trim();
+}
+
+/**
+ * Custom submit function
+ * @param {scope} globals
+ */
+function submitFormArrayToString(globals) {
+  const data = globals.functions.exportData();
+  Object.keys(data).forEach((key) => {
+    if (Array.isArray(data[key])) {
+      data[key] = data[key].join(',');
+    }
+  });
+  globals.functions.submitForm(data, true, 'application/json');
 }
 
 /**
  * Calculate the number of days between two dates.
  * @param {*} endDate
  * @param {*} startDate
- * @return {number} returns the number of days between two dates
+ * @returns {number} returns the number of days between two dates
  */
 function days(endDate, startDate) {
-  return 100;
+  const start = typeof startDate === 'string' ? new Date(startDate) : startDate;
+  const end = typeof endDate === 'string' ? new Date(endDate) : endDate;
+
+  // return zero if dates are valid
+  if (Number.isNaN(start.getTime()) || Number.isNaN(end.getTime())) {
+    return 0;
+  }
+
+  const diffInMs = Math.abs(end.getTime() - start.getTime());
+  return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export { getFullName, days };
+export { getFullName, days, submitFormArrayToString };
